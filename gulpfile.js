20ebcef175d7c5b5990bb4cc6a043d45e489b612
Magomed Suleymanov <gamidovich@mail.ru>
2017-05-11 21:18:19 +0300

    less works

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var scss = require('gulp-scss');
+var style = require('gulp-less');
 var flatten = require('gulp-flatten');
 var svgSprites = require('gulp-svg-sprites');
 var uglify = require('gulp-uglify');
@@ -15,29 +15,34 @@ var pages = require('gulp-gh-pages');
 
  gulp.task("style", function () {
         return gulp.src(
-            "scss/in.scss"
+            "blocks/style.less"
         )
-        .pipe(scss())
-        .pipe(gulp.dest("./"));
+        .pipe(style())
+        .pipe(gulp.dest("css"));
     });
 
 function done() {
 	console.log('Задача выполнена успешно!!!');	
 };
 
- gulp.task("del", function (done) {
-       
-        return del([
-        		"assets"
-        	]);
+gulp.task("del", function () {
+   
+    return del([
+    		"assets"
+    	]);
+});
 
-        done();
 
-    });
+gulp.task("delSome", function (some) {
+   
+    return del([
+    		some
+    	]);
+});
 
  gulp.task("delNM", function () {
         return del([
-        		"node_modules, .gulp-scss-cache"
+        		"node_modules", ".gulp-scss-cache"
         	]);
 
     });
@@ -50,7 +55,7 @@ gulp.task("copy", function() {
 		.pipe(flatten({includeParents: 0}))
 		.pipe(gulp.dest('assets/svg'));
 });
- 
+
 
  gulp.task("pages", function() {
  	gulp.src('dist/**/*')
